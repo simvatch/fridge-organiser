8403fe0ec0d2270cc7e7bf7a9c222eb2ba88b0ea
palebrownbutton <simonavatcheva@hotmail.com>
2026-05-20 19:07:50 +0100

    Linked fronted to backend on login

--- src/Login.jsx
+++ src/Login.jsx
@@ -3,20 +3,38 @@ import { useNavigate, Link } from "react-router-dom";
 import "./Login.css"
 
 export default function Login({ onLogin }) {
-    const [username, setUsername] = useState("")
+    const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const navigate = useNavigate()
 
-    const handleLogin = (e) => {
+    const handleLogin = async (e) => {
         e.preventDefault()
 
-        if (username == "admin" && password == "pass") {
-            onLogin()
-            navigate("/")
-        } else {
-            alert("Invalid username or password!")
+        try {
+            const response = await fetch("https://fridge-organiser.onrender.com/auth/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    email,
+                    password,
+                }),
+            });
+
+            const data = await response.json();
+
+            if (response.ok) {
+                onLogin();
+                navigate("/");
+            } else {
+                alert(data.detail || "Login failed");
+            }
+        } catch (error) {
+            console.log(error)
+            alert("Server error");
         }
-    }
+    };
 
     return (
         <div className="login-container">
@@ -25,12 +43,12 @@ export default function Login({ onLogin }) {
                 <h2>Fridge Login</h2>
                 <form onSubmit={handleLogin}>
                     <div className="input-group">
-                        <label>Username</label>
+                        <label>Email</label>
                         <input
-                            type="text"
-                            value={username}
-                            onChange={(e) => setUsername(e.target.value)}
-                            placeholder="Enter username"
+                            type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            placeholder="Enter email"
                             required
                         />
                     </div>
